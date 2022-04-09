6e411de8fe49adcfa65e770401571911cf4120e4
Bazyli12 <bazyli2006@gmail.com>
2022-04-09 16:34:26 +0200

    da

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use('/src', express.static('public'))
+
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname })
     console.log('dziala')
 })
 
-app.use(express.static('src'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
